6318e807769b3a171f05732ec62c6f48cf10e4d3
Aleksandr Diamond <asdiamond@miners.utep.edu>
2017-10-10 17:18:02 -0600

    Began refractoring everything from linked list stuff to bst stuff. Began reimplementing simple functionality of the bst... Some testing was done on the bst.c and bstdemo.c , but more is needed. Added TODOs.

---
+++ bst.c
@@ -0,0 +1,116 @@
+#include <stdio.h>		/* for puts,  */
+#include <stdlib.h> 		/* for malloc */
+#include <assert.h>		/* for assert */
+#include "bst.h"		
+
+int llDoCheck = 1;		/* set true for paranoid consistency checking */
+//a debug flag
+
+#define doCheck(_lp) (llDoCheck && llCheck(_lp))
+
+/* create a new list */
+BinarySearchTree *BSTAlloc()
+{
+  BinarySearchTree *bst = (BinarySearchTree *)malloc(sizeof(BinarySearchTree));
+  bst->root = 0;
+  //doCheck(lp);
+  return bst;
+}
+
+/* recycle a list, discarding all items it contains */
+void BSTFree(BinarySearchTree *bst)
+{
+	printf("Called BST free\n");
+  //doCheck(lp);
+  BSTMakeEmpty(bst);
+  free(bst);
+}
+
+/* Delete all elements off of the list */
+void BSTMakeEmpty(BinarySearchTree *bst)
+{
+/*
+  LLItem *current = lp->first, *next;
+  doCheck(lp);
+  while (current) {
+    next = current->next;
+    free(current->str);
+    free(current);
+    current = next;
+  }
+  lp->first = lp->last = 0;	/* list is empty */
+ // doCheck(lp);
+	printf("Called BSTMakeEmpty\n");
+}
+  
+/* append a copy of str to end of list */
+//TODO this is gonna be a little more complicated for bst...
+void BSTInsert(BinarySearchTree *bst, char *s)
+{
+	printf("Called BST Insert\n");
+	int len;
+	char *scopy;
+	Node *a;
+
+	for (len = 0; s[len]; len++);//get s length
+
+	scopy = (char *)malloc(len + 1);//malloc char * of size s
+	for(len = 0; s[len]; len++)
+		scopy[len] = s[len];//copy it into scopy
+	scopy[len] = 0;//terminate copy with \0
+
+	a = (Node *)malloc(sizeof(Node));
+	a->str = scopy;
+	a->leftChild = NULL;
+	a->rightChild = NULL;
+	
+	addNode(bst->root, a);//add node to bst
+  	//doCheck(lp);
+}
+
+//TODO test this implementation...
+void addNode(Node *root, Node *a){
+	if(root == NULL){
+		root = a;//a becomes root
+		return;
+	}
+	int cmpval = strcmp(a->str, root->str);
+	if(0 > cmpval) {//a < root
+		addNode(root->leftChild, a);
+	}
+	else {//a >= root
+		addNode(root->rightChild, a);
+	}
+}
+
+/* print list membership.  Prints default mesage if message is NULL */
+void printBST(BinarySearchTree *bst, char *msg)
+{
+	puts(msg ? msg :"BST contents\n");
+	printInorder(bst->root);
+}
+
+/* helper, prints bst in order node by node */
+void printInorder(Node *root){
+	if(root == NULL) return;
+	printInorder(root->leftChild);
+    	printf("<%s>\n", root->str);
+	printInorder(root->rightChild);
+}
+
+/* check llist consistency */
+//TODO reimplement this to make sure bst fulfills its max min stuff.
+/*
+int llCheck(LList *lp)
+{
+  LLItem *ip;
+  ip = lp->first;
+  if (!ip) 
+    assert(lp->last == 0);
+  else {
+    for (; ip->next; ip = ip->next);
+    assert(ip == lp->last);
+  }
+  return 0;
+}
+*/
---
+++ bst.h
@@ -0,0 +1,46 @@
+#ifndef llist_included		/* prevent multiple inclusion */
+#define llist_included
+
+
+/* a BST node with item */
+typedef struct node {
+  struct node *leftChild; 
+  struct node *rightChild; 
+  char *str;
+} Node;
+
+/* a list of LLItems */
+typedef struct {
+	Node *root;
+} BinarySearchTree;
+
+/* Check to see if this is necessary */
+extern int llDoCheck;		/* set true for paranoid consistency checking */
+//debug flag
+
+/* create a new list */
+BinarySearchTree *BSTAlloc();
+
+/* free memory associated with a BST, discarding all items it contains */
+void BSTFree(BinarySearchTree *bst);
+
+/* Delete all elements off list */
+void BSTMakeEmpty(BinarySearchTree *bst);
+
+/* Insert a copy of str into bst */
+void BSTInsert(BinarySearchTree *bst, char *s);
+
+/* Prints the bst... */
+void printBST(BinarySearchTree *bst, char *msg);
+
+/* helper to print bst inorder */
+void printInorder(Node *root);
+
+/* helper to add node to bst */
+void addNode(Node *root, Node *a);
+
+//TODO reimplement this for bst...
+/* check llist consistency, always returns zero */
+//int llCheck(LList *lp);
+
+#endif	/* included */
